f96f14d785f2387a5d4caa392dd0fa3edf73661d
SohamTcog <154597190+SohamTcog@users.noreply.github.com>
2024-01-08 15:45:38 +0530

    Update Todo.tsx

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import AddInput from '../AddInput/AddInput';
-import Header from '../Header/Header';
 import TodoList from '../TodoList/TodoList';
 import "./Todo.css";
 
