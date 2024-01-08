4ddf89777966d77e77c4cd69da86c88c0429ffcc
SohamTcog <154597190+SohamTcog@users.noreply.github.com>
2024-01-08 15:39:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import Banner from './components/Banner/Banner';
 import TodoPage from './pages/TodoPage/TodoPage';
 import FollowersPage from './pages/FollowersPage/FollowersPage';
-import PostListDataContainer from './components/PostList/PostList';
-import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
         <Banner />
         <Routes>
           <Route path="/" element={<TodoPage />} />
           <Route path="/followers" element={<FollowersPage />} />
-          <Route path="/list" element={<PostListDataContainer />} />
         </Routes>
-      </QueryClientProvider>
     </div>
   );
 }
